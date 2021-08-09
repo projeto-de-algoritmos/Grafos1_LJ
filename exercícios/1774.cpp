8a35ccc3618916c6cdd1223c2a712d86d4db4727
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2021-08-08 23:21:50 -0300

    Organizando diretorio e adicionando esqueleto do exercicio 1774.

---
+++ exercícios/1774.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// exercicio resolvido utilizando o algoritmo kruskal
+// criar tipos pros pares e vetor dos pares de nos
+typedef pair<int, int> ii;
+typedef vector< pair<int, ii> > viii;
+
+int main() {
+    
+    return 0;
+}
