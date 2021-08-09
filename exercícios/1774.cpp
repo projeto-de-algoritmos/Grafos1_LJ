bef9c8518228f53fd075d08214fe6c53bba549b2
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2021-08-08 23:24:51 -0300

    Adicionando a estrutura do grafo.

--- exercícios/1774.cpp
+++ exercícios/1774.cpp
@@ -3,10 +3,27 @@
 using namespace std;
 // exercicio resolvido utilizando o algoritmo kruskal
 // criar tipos pros pares e vetor dos pares de nos
-typedef pair<int, int> ii;
-typedef vector< pair<int, ii> > viii;
+typedef pair<int, int> pr;
+typedef vector<pair<int, pr> > vecpr;
+
+// estrutura do grafo
+struct Graph{
+    int v, e;
+    vecpr edges;
+    int kruskal();
+    
+    Graph(int v, int e){
+        this->v = v;
+        this->e = e;
+    }
+
+    void addEdge(int u, int v, int w){
+        edges.push_back(make_pair(w, make_pair(u, v)));
+        edges.push_back(make_pair(w, make_pair(v, u)));
+    }
+};
 
 int main() {
-    
+
     return 0;
 }
